0d5a152110d006f9e0667472209813e88a7f4286
Ali Ahmad <alislearning123@gmail.com>
2020-06-22 00:33:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,12 @@ from yolo import predict
 import cv2 as cv
 import warnings
 
+import random
+
+kk = str(random.randint(1, 100000000))
+
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 image = ""
 motor = 0
@@ -53,7 +58,7 @@ def getupload():
                 print(memory.filename)
                 print("here1")
                 image, mot, hel = predict(memory.filename)
-                cv.imwrite("/static/new_output.jpg", image)
+                cv.imwrite("/static/new_output" + kk +  ".jpg", image)
                 print("here2")
                 motor+=mot
                 helmet+=hel
@@ -84,13 +89,20 @@ def results():
         thing = helmet/motor*100
     except: thing = 0
     
-    return render_template('results.html', memory_vs_time_rf = "/static/new_output.jpg", perfect_rate=thing, forget_rate=motor-helmet)
+    return render_template('results.html', memory_vs_time_rf = "/static/new_output" + kk + ".jpg", perfect_rate=thing, forget_rate=motor-helmet)
     
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+@app.after_request
+def add_header(response):
+    # response.cache_control.no_store = True
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080, debug=True)
